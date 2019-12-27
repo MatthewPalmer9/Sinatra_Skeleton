9a58c8f71f4e7f36f8de817e0a02ea9fbf2bb1a9
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-12-27 03:31:26 +0000

    setting :views within configure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 require_relative  '../../config/environment'
 
 class ApplicationController < Sinatra::Base
-  register Sinatra::ActiveRecordExtension
-  set :views, Proc.new { File.join(root, "../views/") }
+  #register Sinatra::ActiveRecordExtension
+  #set :views, Proc.new { File.join(root, "../views/") }
 
   configure do
+    set :views, "app/views"
     enable :sessions
     set :session_secret, "secret"
   end
