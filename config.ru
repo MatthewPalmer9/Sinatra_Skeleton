f1e2811f061da41e825a781c240347ff40b6e253
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-12-27 04:32:04 +0000

    adding helpful notes/code to application_controller.rb

--- config.ru
+++ config.ru
@@ -1,7 +1,7 @@
 require './config/environment'
 
 begin
-  fi_check_migration
+  # fi_check_migration
 
   use Rack::MethodOverride
 
--- config/environment.rb
+++ config/environment.rb
@@ -7,13 +7,13 @@ configure :development do
   set :database, 'sqlite3:db/database.db' #<- Can name database.rb anything
 end
 
-def fi_check_migration
-  begin
-    ActiveRecord::Migration.check_pending! 
-  rescue ActiveRecord::PendingMigrationError
-    raise ActiveRecord::PendingMigrationError.new "Migrations are pending.\nTo resolve this issue, run: \nrake db:migrate SINATRA_ENV=#{ENV['SINATRA_ENV']}"
-  end
-end
+# def fi_check_migration
+#   begin
+#     ActiveRecord::Migration.check_pending!
+#   rescue ActiveRecord::PendingMigrationError
+#     raise ActiveRecord::PendingMigrationError.new "Migrations are pending.\nTo resolve this issue, run: \nrake db:migrate SINATRA_ENV=#{ENV['SINATRA_ENV']}"
+#   end
+# end
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
