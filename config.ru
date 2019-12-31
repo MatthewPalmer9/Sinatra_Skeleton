e3b5b6808cf3d6ed899afe2b3d2118c001955fe0
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-12-31 19:32:05 +0000

    removed extra code not being used and corrected database path in environment.rb

--- config.ru
+++ config.ru
@@ -1,12 +1,3 @@
 require './config/environment'
 
-begin
-  # fi_check_migration
-
-  use Rack::MethodOverride
-
-  run ApplicationController
-rescue ActiveRecord::PendingMigrationError => err
-  STDERR.puts err
-  exit 1
-end
+run ApplicationController
--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,9 @@
-ENV['SINATRA_ENV'] ||= "development"
-
 require 'bundler'
 Bundler.require
 
-# configure :development do
-# 	set :database, {adapter: "sqlite3", database: "db/database.sqlite3"}
-# end
-
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  :database => "db/development.sqlite"
 )
 
 require_all 'app'
